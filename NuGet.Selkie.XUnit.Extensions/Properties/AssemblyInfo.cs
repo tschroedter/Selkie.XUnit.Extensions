f12543adc8a88f781eabb47aba8c602169d0514d
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2015-05-16 20:55:33 +1000

    Complete Code Cleanup

--- NuGet.Selkie.XUnit.Extensions/Properties/AssemblyInfo.cs
+++ NuGet.Selkie.XUnit.Extensions/Properties/AssemblyInfo.cs
@@ -5,24 +5,24 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("NuGet.Selkie.XUnit.Extensions")]
-[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NuGet.Selkie.XUnit.Extensions")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly : AssemblyTitle("NuGet.Selkie.XUnit.Extensions")]
+[assembly : AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("Microsoft")]
+[assembly : AssemblyProduct("NuGet.Selkie.XUnit.Extensions")]
+[assembly : AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
-[assembly: ComVisible(false)]
+[assembly : ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
+[assembly : Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly : AssemblyVersion("1.0.0.0")]
+[assembly : AssemblyFileVersion("1.0.0.0")]
